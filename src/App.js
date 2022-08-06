b8ee641152ae77e080e3fbe032e4b73161ad9f11
Ahmed Sadman <ahmedsadman.211@gmail.com>
2022-08-06 13:30:12 +0600

    restore changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          This was changed
+          Learn React
         </a>
       </header>
     </div>
