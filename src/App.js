3c7ecf90facd73726b737572b4abed2de6fda1f6
Ahmed Sadman <ahmedsadman.211@gmail.com>
2022-08-06 17:04:31 +0600

    should fail tests

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reacts
+          Hello World
         </a>
       </header>
     </div>
